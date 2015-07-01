f6597341609296a602c5a64323e6bcfc40ab5b0d
Matt Henkel <guildencrantz@menagerie.cc>
2015-06-30 21:31:50 -0600

    Initial intent_schema.js
    Change-Id: I85b6597c303df8c739e8675b8c4feb7456924351

---
+++ intent_schema.js
@@ -0,0 +1,30 @@
+{
+  "intents": [
+    {
+      "intent": "HelpIntent",
+      "slots": []
+    },
+    {
+      "intent": "StatusIntent",
+      "slots": []
+    },
+    {
+      "intent": "SetTempIntent",
+      "slots": [
+        {
+          "name": "Temperature",
+          "type": "NUMBER"
+        }
+      ]
+    },
+    {
+      "intent": "SetPresenceIntent",
+      "slots": [
+        {
+          "name": "Presence",
+          "type": "LITERAL"
+        }
+      ]
+    }
+  ]
+}
