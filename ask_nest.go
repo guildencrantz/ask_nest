2e2add5ef0342336a31c7943862b1bbe92b38344
Matt Henkel <guildencrantz@menagerie.cc>
2015-07-01 22:12:33 -0600

    Basic JSON intent unmarshalling.
    Change-Id: I691339a6a5f961828572fe4cc6af25d0ba906e0a

---
+++ ask_nest.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Session struct {
+	New bool `json:"new"`
+}
+
+type Intent struct {
+	Name  string                            `json:"name"`
+	Slots map[string]map[string]interface{} `json:"slots"`
+}
+
+type Request struct {
+	Type   string `json:"type"`
+	Id     string `json:"requestId"`
+	Intent Intent `json:"intent"`
+}
+
+type Event struct {
+	Session Session `json:"session"`
+	Request Request `json:"request"`
+}
+
+func main() {
+	input := os.Args[1]
+	fmt.Println(input)
+
+	var event Event
+	json.Unmarshal([]byte(input), &event)
+	fmt.Println("%q", event)
+	fmt.Println(event.Request.Intent.Slots["Color"]["value"])
+}
